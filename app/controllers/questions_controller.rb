bfbeaf6bf2406deae1a80ad9a76f044579886d6f
Dominic <jxdlam@gmail.com>
2016-06-29 15:43:25 +0800

    Updated feature to create questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,6 +6,7 @@ class QuestionsController < ApplicationController
   end
 
   def index
+    @myqns = Question.where(user_id: current_user)
   end
 
   def show
@@ -16,10 +17,23 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    #code
+    @qns = Question.new(question_params)
+    @qns.user = current_user
+
+    if @qns.save
+      redirect_to home_path, notice: "Successfully created!"
+    else
+      render "new", notice: "Couldn't create question. Please contact administrator."
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :questions)
   end
 
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
   belongs_to :user
 
-  validates :questions, presence: true
+  validates :questions, :title, presence: true
 end
