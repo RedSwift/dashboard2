50c071b1aee991c0a1e4435dd7c40413d2565b9e
Dominic <jxdlam@gmail.com>
2016-06-29 14:33:48 +0800

    Updated login feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to home_path, notice: 'Welcome, #{user.id}'
+    else
+      render "new", notice: "Invalid login details"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+  get 'questions/index'
+
+  get 'questions/show'
+
+  get 'questions/new'
+
+  get 'questions/edit'
+
   root 'sessions#new'
   # post 'sign_up', to: 'users#create'
 
-  resources :users, only: [:create, :new]
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
