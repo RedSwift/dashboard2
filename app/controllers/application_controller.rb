feda5c219e2c959d51a62f57d21c61dd693ee773
Dominic <jxdlam@gmail.com>
2016-06-30 18:03:00 +0800

    Minor display adjustments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :logged_in?
   helper_method :is_admin?
+  helper_method :admin_only
 
   def current_user
     @c_user ||= User.find(session[:user_id]) if session[:user_id]
